f09e2cd59041430e7503dbd07202af1c2a2c1bbe
Zulpikar <zulpikaruygur@cybertekschool.com>
2022-06-16 01:19:29 +0300

    pom updated

--- src/test/java/FastTrack3Serenity/SpartanTest.java
+++ src/test/java/FastTrack3Serenity/SpartanTest.java
@@ -1,2 +1,13 @@
-package FastTrack3Serenity;public class SpartanTest {
+package FastTrack3Serenity;
+
+public class SpartanTest {
+
+
+
+
+    // get all spartans
+    // https://github.com/zulpikarCydeo/FastTrack3Serenity.git
+    // use rest given first, then serenity given
+    // run from test method, then run from maven or terminal, see different
+
 }
